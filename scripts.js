62d97a274549c3a33dd974fd96575350e214440c
Francois Romain <francoisromain@gmail.com>
2019-08-13 11:35:52 +0200

    feat: add vuejs

--- scripts.js
+++ scripts.js
@@ -1,33 +1,51 @@
-// define map and tile layer
-const map = L.map("mapid", {
-  doubleClickZoom: false,
-  minZoom: 4,
-  gestureHandling: true,
-  fullscreenControl: {
-    pseudoFullscreen: true
-  }
-}).setView([51.505, -0.09], 13);
+var app = new Vue({
+  el: "#app",
+  data() {
+    return {
+      message: "Hello Vue!",
+      map: null
+    };
+  },
 
-const scale = L.control.scale({ imperial: false });
+  mounted() {
+    this.init();
+  },
 
-const tileLayer = L.tileLayer(
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  {
-    maxZoom: 20,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }
-);
+  methods: {
+    init() {
+      // define map and tile layer
+      this.map = L.map(this.$refs.map, {
+        doubleClickZoom: false,
+        minZoom: 4,
+        gestureHandling: true,
+        fullscreenControl: {
+          pseudoFullscreen: true
+        }
+      }).setView([51.505, -0.09], 13);
 
-scale.addTo(map);
-tileLayer.addTo(map);
+      const scale = L.control.scale({ imperial: false });
 
-map.on("fullscreenchange", () => {
-  if (map.isFullscreen()) {
-    map.gestureHandling.disable();
-    console.log("entered fullscreen", map.gestureHandling.enabled());
-  } else {
-    map.gestureHandling.enable();
-    console.log("exited fullscreen", map.gestureHandling);
+      const tileLayer = L.tileLayer(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        {
+          maxZoom: 20,
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }
+      );
+
+      scale.addTo(this.map);
+      tileLayer.addTo(this.map);
+
+      this.map.on("fullscreenchange", () => {
+        if (this.map.isFullscreen()) {
+          this.map.gestureHandling.disable();
+          console.log("entered fullscreen", this.map.gestureHandling.enabled());
+        } else {
+          this.map.gestureHandling.enable();
+          console.log("exited fullscreen", this.map.gestureHandling);
+        }
+      });
+    }
   }
 });
